d0adef609b37daa3b492a39d06bafb5ec5c41884
Rich Ramos <richard.ramos3124@gmail.com>
2021-09-07 23:57:15 -0400

    Renamed decorator file

---
+++ PoseQBO/wwwroot/js/invoice-form/decorators/SearchByNameAndDateInvoiceFormStateDecorator.js
@@ -0,0 +1,34 @@
+﻿import InvoiceFormStateDecoratorBase from './base/InvoiceFormStateDecoratorBase.js';
+
+class SearchByNameAndDateInvoiceFormStateDecorator extends InvoiceFormStateDecoratorBase {
+    constructor(invoiceFormState) {
+        super(invoiceFormState);
+        const newFormState =
+            `
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="companyName">Company Name</label>
+                        <input type="text" name="companyName" class="form-control" placeHolder="Name of company" />
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="startDate">Start Date</label>
+                        <input type="date" name="startDate" class="form-control" />
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="endDate">End Date</label>
+                        <input type="date" name="endDate" class="form-control" />
+                    </div>
+                </div>
+                <div class="text-center m-2">
+                    <button class="btn btn-primary text-center" type="submit">Submit</button>
+                </div>
+            `;
+        this.invoiceFormState.State = newFormState;
+    }
+
+    get State() {
+        return super.State;
+    }
+}
+
+export default SearchByNameAndDateInvoiceFormStateDecorator;
