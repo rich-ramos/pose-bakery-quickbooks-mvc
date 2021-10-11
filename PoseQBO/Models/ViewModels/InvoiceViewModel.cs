9f2de288e5d63894bafdb8216ed40a2578ee6cbd
Rich Ramos <richard.ramos3124@gmail.com>
2021-10-11 16:52:24 -0400

    Added view model for Invoice

---
+++ PoseQBO/Models/ViewModels/InvoiceViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Intuit.Ipp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoseQBO.Models.ViewModels
+{
+    public class InvoiceViewModel
+    {
+        public Invoice Invoice { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
