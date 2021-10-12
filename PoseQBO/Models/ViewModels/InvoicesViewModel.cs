bdb24198e08b46b2100b2e5a0ccc27c13ac6b535
Rich Ramos <richard.ramos3124@gmail.com>
2021-10-11 22:25:35 -0400

    Added get method to property ItemByQuantity

--- PoseQBO/Models/ViewModels/InvoicesViewModel.cs
+++ PoseQBO/Models/ViewModels/InvoicesViewModel.cs
@@ -12,6 +12,52 @@ namespace PoseQBO.Models.ViewModels
         public IEnumerable<Invoice> Invoices { get; set; }
         public string StartDate { get; set; }
         public string EndDate { get; set; }
-        public IDictionary<string, int> ItemByQuantity { get; set; }
+        public IDictionary<string, int> ItemByQuantity
+        {
+            get
+            {
+                var itemByQuantityMap = new Dictionary<string, int>();
+
+                void GetLinesFromInvoice(IEnumerable<Invoice> invoices)
+                {
+                    if (invoices.Count() == 0)
+                    {
+                        return;
+                    }
+
+                    var lines = invoices.First().Line;
+
+                    void CollectLineValues(IEnumerable<Line> lines)
+                    {
+                        if (lines.Count() == 0)
+                        {
+                            return;
+                        }
+
+                        if (lines.First().AnyIntuitObject is SalesItemLineDetail lineItem)
+                        {
+                            var itemName = lineItem.ItemRef.name;
+                            var itemQyt = ((int)lineItem.Qty);
+
+                            if (!itemByQuantityMap.ContainsKey(itemName))
+                            {
+                                itemByQuantityMap.Add(itemName, itemQyt);
+                            }
+                            else
+                            {
+                                itemByQuantityMap[itemName] += itemQyt;
+                            }
+                        }
+                        CollectLineValues(lines.Skip(1));
+                    }
+                    CollectLineValues(lines);
+
+                    GetLinesFromInvoice(invoices.Skip(1));
+                }
+                GetLinesFromInvoice(Invoices);
+
+                return itemByQuantityMap;
+            }
+        }
     }
 }
