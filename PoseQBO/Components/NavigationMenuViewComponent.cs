8c898a48a8f48b35af2df9da90a1ba642e59b07f
Rich Ramos <richard.ramos3124@gmail.com>
2021-10-20 10:42:19 -0400

    Created View Component for navigation menu.

---
+++ PoseQBO/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoseQBO.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+
+        public NavigationMenuViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
