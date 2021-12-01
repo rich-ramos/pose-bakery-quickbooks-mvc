f2c07ba30c6e253d6d7b4e8722dcb3cc89b9a94b
Rich Ramos <richard.ramos3124@gmail.com>
2021-12-01 15:05:12 -0500

    Implemented user login/logout feature

---
+++ PoseQBO/Controllors/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PoseQBO.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoseQBO.Controllors
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private SignInManager<IdentityUser> _signInManager;
+        private UserManager<IdentityUser> _userManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result =
+                    await _signInManager.PasswordSignInAsync(model.UserName, model.Password,
+                    false, false);
+                if (result.Succeeded)
+                {
+                    return Redirect(model.ReturnUrl ?? "/");
+                }
+                ModelState.AddModelError("", "Invalid username or password");
+            }
+            return View(model);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
---
+++ PoseQBO/Models/ViewModels/AccountViewModels.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoseQBO.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+
+    public class DetailsModel
+    {
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
