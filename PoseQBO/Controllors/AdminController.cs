1d312937840bef3ebfe7fe196de352febbb911b5
Rich Ramos <richard.ramos3124@gmail.com>
2021-12-01 14:57:29 -0500

    Added 'Admins' role to class scope to authorize controllers'

--- PoseQBO/Controllors/AdminController.cs
+++ PoseQBO/Controllors/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PoseQBO.Models.ViewModels;
 using System;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace PoseQBO.Controllors
 {
+    [Authorize(Roles = "Admins")]
     public class AdminController : Controller
     {
         private UserManager<IdentityUser> _userManager;
--- PoseQBO/Controllors/RolesController.cs
+++ PoseQBO/Controllors/RolesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PoseQBO.Models.ViewModels;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace PoseQBO.Controllors
 {
+    [Authorize(Roles = "Admins")]
     public class RolesController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
