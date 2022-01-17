e9598aefe487065e9f66bd99467b8023047259b2
Rich Ramos <richard.ramos3124@gmail.com>
2022-01-17 16:10:23 -0500

    Implemented loading spinner

---
+++ PoseQBO/wwwroot/js/modules/LoadingSpinner.js
@@ -0,0 +1,26 @@
+﻿class LoadingSpinner {
+    constructor() {
+        this.spinner = document.querySelector('.lds-ring');
+        this.overlay = document.querySelector('.overlay');
+        this.#events();
+    }
+
+    #events() {
+        document.addEventListener('DOMContentLoaded', (e) => this.#handleContentLoaded(e));
+    }
+
+    #handleContentLoaded(e) {
+        this.hideSpinner();
+    }
+
+    hideSpinner() {
+        this.spinner.style.display = 'none';
+        this.overlay.classList.remove('overlay');
+    }
+
+    showSpinner() {
+        this.spinner.style.display = 'block';
+        this.overlay.classList.add('overlay');
+    }
+}
+export default LoadingSpinner;
